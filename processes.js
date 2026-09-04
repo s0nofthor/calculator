395276c79e5318b5cbeb09cbb2ae4c3a8235d1dd
s0nofthor <larsthomast@gmail.com>
2026-09-04 14:11:46 -0400

    Allow most buttons to interact with display

--- processes.js
+++ processes.js
@@ -1,16 +1,16 @@
 //Identify Parts
 const device = document.getElementById('container');
     const display = document.getElementById('display');
-        let num1 = display.appendChild(document.createElement('div'));
-        let op = display.appendChild(document.createElement('div'));
-        let num2 = display.appendChild(document.createElement('div'));
-    const button = display.querySelectorAll('button');
+        let num1 = display.appendChild(document.getElementById('x'));
+        let oprtn = display.appendChild(document.getElementById('op'));
+        let num2 = display.appendChild(document.getElementById('y'));
+    const button = device.querySelectorAll('button');
     const numBox = document.getElementById('number-inputs');
         const nums = numBox.querySelector('button');
     const opBox = document.getElementById('in-betweens');
         const ops = opBox.querySelector('button');
-    const eraserBox = document.getElementById('erasers');
-        const erasers = eraserBox.querySelector('button');
+    const bigChangeBox = document.getElementById('transformers');
+        const bigChanges = bigChangeBox.querySelector('button');
 //Create Operations
 function add(a, b) {
     return a + b;
@@ -36,3 +36,21 @@ function divide(a, b) {
             return divide(num1, num2);
         }
     }
+//Input Triggers
+button.forEach(btn => {
+    btn.addEventListener('click', event => {
+        if (btn.className === 'num' && op.innerHTML === '') {
+            let x = document.createElement('div');
+            x.innerHTML = btn.innerHTML;
+            num1.appendChild(x);
+        } else if (num1.innerHTML !== '' && btn.className === 'op') {
+            let op = document.createElement('div');
+            op.innerHTML = btn.innerHTML;
+            oprtn.appendChild(op);
+        } else if (oprtn.innerHTML !== '' && btn.className === 'num') {
+            let y = document.createElement('div');
+            y.innerHTML = btn.innerHTML;
+            num2.appendChild(y);
+        }
+    })
+})
